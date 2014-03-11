2f8ecdafdb2816906665a4939c47904814d5328a
James Sumners <james.sumners@clayton.edu>
2014-03-11 13:29:36 -0400

    Attempts to fix issues with extended character sets and Hibernate
    Hibernate defaults to using whatever encoding the JVM is set to. You
    can configure the encoding that Hibernate uses for most queries, but
    you cannot configure the encoding hbm2ddl uses for creating schemas.
    This commit fixes it in places where it can and adds a note where it
    can't.

--- src/main/java/com/jrfom/icelotto/config/DevDataSouceConfig.java
+++ src/main/java/com/jrfom/icelotto/config/DevDataSouceConfig.java
@@ -34,6 +34,7 @@ public class DevDataSouceConfig implements DataSourceConfig {
       dataSource = new SQLiteDataSource();
       ((SQLiteDataSource) dataSource).setDatabaseName(this.env.getRequiredProperty("db.name"));
       ((SQLiteDataSource) dataSource).setUrl(this.env.getRequiredProperty("db.url"));
+      ((SQLiteDataSource) dataSource).setEncoding("UTF-8");
     }
 
     return dataSource;
@@ -50,6 +51,10 @@ public class DevDataSouceConfig implements DataSourceConfig {
     jpaProperties.put("hibernate.hbm2ddl.auto", this.env.getProperty("hibernate.hbm2ddl.auto"));
     jpaProperties.put("hibernate.hbm2ddl.import_files", this.env.getProperty("hibernate.hbm2ddl.import_files"));
     jpaProperties.put("hibernate.hbm2ddl.import_files_sql_extractor", "org.hibernate.tool.hbm2ddl.MultipleLinesSqlCommandExtractor");
+
+    jpaProperties.put("hibernate.connection.CharSet", "utf8");
+    jpaProperties.put("hibernate.connection.characterEncoding", "utf8");
+    jpaProperties.put("hibernate.connection.useUnicode", "true");
 
     // H2 database needs this next one
     String schema = this.env.getProperty("hibernate.default_schema");
--- src/main/java/com/jrfom/icelotto/config/ProdDataSouceConfig.java
+++ src/main/java/com/jrfom/icelotto/config/ProdDataSouceConfig.java
@@ -30,6 +30,7 @@ public class ProdDataSouceConfig implements DataSourceConfig {
       dataSource = new SQLiteDataSource();
       ((SQLiteDataSource) dataSource).setDatabaseName(this.env.getRequiredProperty("db.name"));
       ((SQLiteDataSource) dataSource).setUrl(this.env.getRequiredProperty("db.url"));
+      ((SQLiteDataSource) dataSource).setEncoding("UTF-8");
     }
 
     return dataSource;
@@ -46,6 +47,10 @@ public class ProdDataSouceConfig implements DataSourceConfig {
     jpaProperties.put("hibernate.hbm2ddl.auto", this.env.getProperty("hibernate.hbm2ddl.auto"));
     jpaProperties.put("hibernate.hbm2ddl.import_files", this.env.getProperty("hibernate.hbm2ddl.import_files"));
     jpaProperties.put("hibernate.hbm2ddl.import_files_sql_extractor", "org.hibernate.tool.hbm2ddl.MultipleLinesSqlCommandExtractor");
+
+    jpaProperties.put("hibernate.connection.CharSet", "utf8");
+    jpaProperties.put("hibernate.connection.characterEncoding", "utf8");
+    jpaProperties.put("hibernate.connection.useUnicode", "true");
 
     // H2 database needs this next one
     String schema = this.env.getProperty("hibernate.default_schema");
